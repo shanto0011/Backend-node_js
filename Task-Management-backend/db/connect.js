d90473a33651da0b5f71ec0858aa56028d828f70
MD.  Shanto <135007992+shanto0011@users.noreply.github.com>
2023-12-30 20:58:03 +0600

    Add files via upload

--- Task-Management-backend/db/connect.js
+++ Task-Management-backend/db/connect.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose')
 
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+            /* useNewUrlParser: true, */
+            useUnifiedTopology: true,
+        })
+        .then(() => console.log('Server Connected with database sucessfully'))
+        .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
