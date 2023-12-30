93ea3ef357ce89e63a24b9059ec875941e7fc199
MD.  Shanto <135007992+shanto0011@users.noreply.github.com>
2023-12-30 20:55:19 +0600

    Add files via upload

--- Task-Management-backend/middleware/async.js
+++ Task-Management-backend/middleware/async.js
@@ -1,1 +1,22 @@
+const asyncWrapper = (fn) => {
+    return async(req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
 
+/* 
+const getTask = asyncWrapper(async(req, res, next) => {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
+    }
+
+    res.status(200).json({ task })
+}) */
+
+module.exports = asyncWrapper
